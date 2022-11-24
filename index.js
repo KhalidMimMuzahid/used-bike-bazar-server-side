b93b0e276b708aac7eabcc443011a4d925af3d78
Khalid Mim Muzahid <khalidmimm@gmail.com>
2022-11-24 18:47:53 +0600

    user saved to database

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const cors = require("cors");
 // const jwt = require("jsonwebtoken");
-// require("dotenv").config();
-// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.port || 5000;
 app.use(cors());
 app.use(express.json());
-// const user = process.env.DB_USER;
-// const password = process.env.DB_PASSWORD;
-// const uri = `mongodb+srv://${user}:${password}@cluster0.sm41jne.mongodb.net/?retryWrites=true&w=majority`;
-
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+const uri = `mongodb+srv://${user}:${password}@cluster0.g5jzoct.mongodb.net/?retryWrites=true&w=majority`;
 app.get("/", async (req, res) => {
   res.send("okk boss");
 });
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("users").collection("users");
+
+    app.post("/user", async (req, res) => {
+      const userInfo = req.body;
+      console.log(userInfo);
+
+      const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.log());
+
 app.listen(port, () => {
   console.log("listening on port", port);
 });
